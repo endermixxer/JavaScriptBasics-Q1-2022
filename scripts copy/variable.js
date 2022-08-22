5418ef80b4fa386f69d52007af908204366591f5
Evan Gerstner <110943564+endermixxer@users.noreply.github.com>
2022-08-22 16:23:39 -0600

    Made my own lol

---
+++ scripts copy/variable.js
@@ -0,0 +1,42 @@
+// @ts-check
+
+//let 
+let gpa = 15;
+let weight = 106;
+let gpaweight = weight/gpa
+
+//constant, cannont change
+
+const myname = "Evan Gerstner";
+//myname = +1
+
+
+
+//boolean variable
+
+
+let isThiscClassAwesome = true;
+
+
+console.log(myname);
+
+    let car = {
+        year: 2024,
+        make: "honda",
+            model: "sivic",
+            isCleanCurrently: false,
+    };
+    console.log(car.year);
+    car.color = "Grey";
+    console.log(car);
+
+
+    // declare array of fruits
+    let fruits = [
+        "apple",
+        "pear",
+        "grapes",
+        "banana",
+
+    ]
+console.log(fruits[2]);
