0ab18c7fcf169c6de8aaf17fcfaab3bf672d80eb
evan gerstner <evan.gerstner@gmail.com>
2022-08-24 19:31:24 -0600

    Changed names and added some code

--- scripts/guessing-game.js
+++ scripts/guessing-game.js
@@ -10,26 +10,30 @@ button?.addEventListener("click", function () {
 	// multiply it by 10 and add then get the whole
 	// number using floor.
 	let answer = Math.floor(Math.random() * 200 + 1);
-	let numberOfGuesses = 3;
+	let numberOfGuesses = 10;
 	let usedGuesses = 1;
 	let isGameOver = false;
 
 	while (isGameOver == false) {
-		let guess = Number(prompt("Pick a number between 1 and 10"));
+		
 		usedGuesses = usedGuesses + 1;
-
-		if (answer == guess) {
-			alert("You won!");
-			isGameOver = true;
-		} else if (usedGuesses > numberOfGuesses) {
+		if (usedGuesses > numberOfGuesses) {
 			alert("You lose! No more guesses!");
 			isGameOver = true;
-		} else {
-			if (guess < answer) {
-				alert("Too low! Guess again.");
-			} else {
-				alert("Too high! Guess again.");
+		}  else {
+			let guess = Number(prompt("Pick a number between 1 and 200"));
+			if (answer == guess) {
+				alert("You won!");
+				isGameOver = true;
+			}  else {
+				if (guess < answer) {
+					alert("Too low! ");
+				} else {
+					alert("Too high! ");
+				}
 			}
 		}
-	}
-});
+
+		
+	
+}});
